09c5ad7eb1299cbfc0fe9985d23e264d111d9e35
theBoxed <bradycfox@gmail.com>
2018-10-08 15:44:21 -0400

    returning list of notes - completed

--- server.js
+++ server.js
@@ -5,4 +5,20 @@ const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
+const express = require('express');
+
+const app = express();
+
 // INSERT EXPRESS APP CODE HERE...
+
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+})
+
+app.listen(8080, function() {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
