af18af11be03c296766a11cd822d4185f679251f
theBoxed <bradycfox@gmail.com>
2018-10-09 15:12:36 -0400

    added error handling

--- server.js
+++ server.js
@@ -44,7 +44,25 @@ app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   let requestedData = data.find(item => item.id === Number(id));
   res.json(requestedData);
-})
+});
+
+//Error-handling
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({message: 'Not Found'});
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ 
+    message: err.message,
+    error: err
+  });
+});
+
+//LISTEN
 
 app.listen(PORT, function() {
   console.info(`Server listening on ${this.address().port}`);
