ce730b02e0d30ed65a533cdcac29ad865ca584a6
theBoxed <bradycfox@gmail.com>
2018-10-11 11:53:38 -0400

    converted notes.create to a promise

--- router/notes.router.js
+++ router/notes.router.js
@@ -76,16 +76,18 @@ router.put('/notes/:id', (req, res, next) => {
       err.status = 400;
       return next(err);
     }
-    notes.create(newItem, (err, item) => {
-      if (err) {
-        return next(err);
-      }
-      if (item) {
-        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-      } else {
-        next ();
-      }
-    })
+
+    notes.create(newItem)
+      .then(item => {
+        if(item) {
+          res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+        } else {
+          next();
+        }
+      })
+      .catch(err => {
+        next(err);
+      })
   });
 
 
