0055c7b9ef9bc233cf71ce251e3cd34d1d21bc3c
Box <bradycfox@gmail.com>
2018-10-14 15:50:30 -0400

    added all endpoint tests

--- test/server.test.js
+++ test/server.test.js
@@ -94,3 +94,116 @@ describe('GET /api/notes', function () {
   });
 });
 //END /api/notes test
+
+describe('POST /api/notes/:id', function() {
+  it('should return correct note object', function() {
+    let noteId = 1001;
+    return chai.request(app)
+      .get(`/api/notes/${noteId}`)
+      .then(res => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.all.keys('id', 'title','content');
+        expect(res.body.id).to.equal(noteId);
+      });
+  });
+
+  it('should respond with a 404 for an invalid id', function() {
+    let noteId = 2001;
+    return chai.request(app)
+      .get(`/api/notes/${noteId}`)
+      .then(res => {
+        expect(res).to.have.status(404);
+        expect(res.body).to.have.keys('message');
+      });
+  });
+});
+//END /api/notes/:id test
+
+describe('POST /api/notes', function() {
+  it('should create and return a new item with location header when provided valid data', function() {
+    const newNote = {title: 'our test note', content: 'test note body content1'};
+    return chai.request(app)
+      .post('/api/notes')
+      .send(newNote)
+      .then(res => {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'title', 'content');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newNote, {id: res.body.id}));
+      });
+  });
+  it('should return an object with a message property "Missing title in request body" when missing "title" field' , function() {
+    const newNote = {content: 'test note body content1'};
+    return chai.request(app)
+      .post('/api/notes')
+      .send(newNote)
+      .then(res => {
+        expect(res).to.have.status(400);
+        expect(res.body.message).to.be.string('Missing `title` in request body');
+      });
+  });
+
+});
+//END POST /api/notes
+
+describe('PUT/api/notes/:id', function(){
+  it('should update and return a note object when given valid data', function(){
+    const updateNote = { title:'boring15', content:'more content to test with eh' };
+    return chai.request(app)
+      .get('/api/notes')
+      .then(function(res){
+        updateNote.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/api/notes/${updateNote.id}`)
+          .send(updateNote);
+      })
+      .then(function(res){
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res).to.be.a('object');
+        expect(res.body).to.deep.equal(updateNote);
+      });
+
+  });
+
+  it('should respond with a 404 for an invalid id', function(){
+    const updateNote = { id:2010, title:'borin16', content:'hey, we just testing over here.' };
+    return chai.request(app)
+      .put(`/api/notes/${updateNote.id}`)
+      .send(updateNote)
+      .then(function(res){
+        expect(res).to.have.status(404);
+      });
+  });
+
+  it('should return an object with a message property "Missing `title` in request body" when missing "title" field', function(){
+    const updateNote = {id:'1015', title: '', content:'we testin and having fun!' };
+    return chai.request(app)
+      .get('/api/notes')
+      .then(function(res){
+        updateNote.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/api/notes/${updateNote.id}`)
+          .send(updateNote);
+      })
+      .then(function(res){
+        expect(res.body.message).to.be.string('Missing `title` in request body');
+      });
+  });
+
+});
+
+describe('DELETE/api/notes/:id', function(){
+  it('should delete item by id', function(){
+    return chai.request(app)
+      .get('/api/notes/')
+      .then(function(res){
+        return chai.request(app).delete(`/api/notes/${res.body[0].id}`);
+      })
+      .then(function(res){
+        expect(res).to.have.status(204);
+      });
+  });
+});
